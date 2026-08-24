bfc48334c0d9d4885cfb267cbdee22c87b1d6d89
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-24 22:47:22 +0200

    Refactoring.

--- .github/scripts/ci.py
+++ .github/scripts/ci.py
@@ -8,7 +8,7 @@ import subprocess
 import sys
 from pathlib import Path
 
-#ROOT = Path(__file__).resolve().parents[2]
+ROOT = Path(__file__).resolve().parents[2]
 #sys.path.insert(0, str(ROOT))
 
 from build import DISPLAY_NAME
