ecd67c54286ff548d1298795633d1ff1a1ab5e43
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-24 22:46:44 +0200

    Refactoring.

--- .github/scripts/ci.py
+++ .github/scripts/ci.py
@@ -8,8 +8,8 @@ import subprocess
 import sys
 from pathlib import Path
 
-ROOT = Path(__file__).resolve().parents[2]
-sys.path.insert(0, str(ROOT))
+#ROOT = Path(__file__).resolve().parents[2]
+#sys.path.insert(0, str(ROOT))
 
 from build import DISPLAY_NAME
 from build import MACOS_TARGETS
