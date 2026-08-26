381e99408269a3928acee243e81c39fc0b5f9de3
Erik Moqvist <erik.moqvist@gmail.com>
2026-08-26 11:30:16 +0200

    Remove unix cfg.

--- crates/virtualcam/src/main.rs
+++ crates/virtualcam/src/main.rs
@@ -1,31 +1,11 @@
-#[cfg(unix)]
 mod alsa;
-#[cfg(unix)]
 mod audio;
-#[cfg(unix)]
 mod camera;
-#[cfg(unix)]
 mod convert;
-#[cfg(unix)]
 mod options;
-#[cfg(unix)]
 mod pulse;
-#[cfg(unix)]
 mod v4l2;
 
-const UNSUPPORTED: &str = "mobcam-virtualcam is only supported on Linux";
-
-#[cfg(unix)]
 fn main() -> std::process::ExitCode {
-    if !cfg!(target_os = "linux") {
-        eprintln!("{UNSUPPORTED}");
-        return std::process::ExitCode::FAILURE;
-    }
     camera::main()
-}
-
-#[cfg(not(unix))]
-fn main() -> std::process::ExitCode {
-    eprintln!("{UNSUPPORTED}");
-    std::process::ExitCode::FAILURE
 }
